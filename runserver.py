ca0a11608a01ae16f6224102ebfc5bc2081e07fd
Yasir Aris <git@yasir.id>
2025-07-04 10:11:16 +0700

    Finishing
    Signed-off-by: Yasir Aris <git@yasir.id>

--- runserver.py
+++ runserver.py
@@ -1,7 +1,7 @@
-import uvicorn
+import gunicorn
 import os
 
 port = os.environ.get("PORT", 80)
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=int(port), reload=False)
+    gunicorn.run("app:app", host="0.0.0.0", port=int(port), reload=False)
